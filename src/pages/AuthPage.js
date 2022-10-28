1d367d4d91cd9b8c9a1fa1a5e54dcd58c8ba89f8
Azer_G <azer.gurbanow@gmail.com>
2022-10-28 17:31:29 +0400

    Second Update

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 
 const AuthPage = () => {
   const context = useContext(AuthContext);
-  console.log(context);
+  // console.log(context);
   return <AuthForm />;
 };
 
